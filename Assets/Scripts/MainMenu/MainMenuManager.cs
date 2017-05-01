6418d6af17fa61e80b1a99fd68b30b57135e27b0
Ryanthekid22 <Formiatti@gmail.com>
2017-05-01 23:10:39 +1000

    Added MainMenu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+		Debug.Log("LOADING");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+		Debug.Log("Exiting");
+	}
+
+}

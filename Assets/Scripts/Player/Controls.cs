fa55b4cdcdea5eaf665d8386c4cc8cee0b152e18
Ethan Johnston <rocketstrong600@gmail.com>
2017-05-02 17:29:37 +1000

    inventory
    inventory code added

---
+++ Assets/Scripts/Player/Controls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+	public Item item;
+	public Item item2;
+	private Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+		inventory = FindObjectOfType<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("t")) {
+			Debug.Log("adding test item");
+			inventory.AddItem (item);
+		}
+		if (Input.GetKeyDown ("r")) {
+			Debug.Log("removing test item");
+			inventory.RemoveItem(item);
+		}
+		if (Input.GetKeyDown ("y")) {
+			Debug.Log("adding test item");
+			inventory.AddItem (item2);
+		}
+		if (Input.GetKeyDown ("u")) {
+			Debug.Log("removing test item");
+			inventory.RemoveItem(item2);
+		}
+	}
+}
